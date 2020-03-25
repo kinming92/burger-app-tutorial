1cf4a1eb5cc1aa7c5f873bf61cdf014f1d4d5f62
kinming92 <kinming.klm@gmail.com>
2020-03-25 01:35:55 -0700

    working on backdrop

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,11 @@ import classes from './Burger.css'
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) =>{
-    let transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => { 
-                return <BurgerIngredient key={igKey + i} type={igKey} />
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => { 
+            return <BurgerIngredient key={igKey + i} type={igKey} />
             }); //[two element]
-        }) .reduce( (arr, el) =>{
+        }).reduce( (arr, el) =>{
             return arr.concat(el);
         }, []);
     console.log(transformedIngredients);
