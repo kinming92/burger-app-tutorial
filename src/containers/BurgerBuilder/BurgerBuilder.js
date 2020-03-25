ad3cdfce7bd3443cb9d2b2a78f7641148accf74f
kinming92 <kinming.klm@gmail.com>
2020-03-25 00:51:37 -0700

    working on the mobile screen

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,6 +72,12 @@ class BurgerBuilder extends Component {
     purchaseHandler = () => {
         this.setState({purchasing: true});
     }
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false});
+    }
+    purchaseContinueHandler = () => {
+        alert('You continue');
+    }
     render () {
         const disabledInfo = {
             ...this.state.ingredients
@@ -81,8 +87,12 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal show={this.state.purchasing}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary 
+                      ingredients={this.state.ingredients}
+                      price={this.state.totalPrice}
+                      purchaseCanceled={this.purchaseCancelHandler} 
+                      purchaseContinued={this.purchaseContinueHandler} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
