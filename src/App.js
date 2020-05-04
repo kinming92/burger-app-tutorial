aafa92178ec3de4a1102ef109b4996691814e20f
kinming92 <kinming.klm@gmail.com>
2020-05-04 03:34:41 -0700

    done with switching to functional component in hoc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import {connect } from 'react-redux';
-import asynComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
@@ -10,61 +9,52 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 // import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
   return import('./containers/Checkout/Checkout');
 });
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import('./containers/Orders/Orders');
 });
 
-const asyncAuth = asynComponent(() => {
+const Auth = React.lazy(() => {
   return import('./containers/Auth/Auth');
 });
 
-class App extends Component {
-  // state = {
-  //   show: true
-  // }
-  // componentDidMount () {
-  //   setTimeout( () => {
-  //     this.setState({show: false});
-  //   }, 5000);
-  // }
-  componentDidMount() {
-    this.props.onTryAutoSignup();
-  }
-  render() {
-    let routes = (
+const app = props => {
+
+  useEffect(() => {
+    props.onTryAutoSignup();
+  }, []); 
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth}   />
+      <Route path="/" exact component={BurgerBuilder}   />
+      <Redirect to="/" />
+    </Switch>
+  );
+  if(props.isAuthenticated){
+    routes = (
       <Switch>
-        <Route path="/auth" component={asyncAuth}   />
-        <Route path="/" exact component={BurgerBuilder}   />
-        <Redirect to="/" />
+          <Route path="/checkout" component={Checkout}   />
+          <Route path="/orders" component={Orders}   />
+          <Route path="/logout" component={Logout}   />
+          <Route path="/auth" component={Auth}   />
+          <Route path="/" component={BurgerBuilder}   />
+          <Redirect to="/" />
       </Switch>
     );
-    if(this.props.isAuthenticated){
-      routes = (
-        <Switch>
-            <Route path="/checkout" component={asyncCheckout}   />
-            <Route path="/orders" component={asyncOrders}   />
-            <Route path="/logout" component={Logout}   />
-            <Route path="/auth" component={asyncAuth}   />
-            <Route path="/" component={BurgerBuilder}   />
-            <Redirect to="/" />
-        </Switch>
-      );
-    }
-    return (
-      <div>
-        <Layout>
-          {routes}
-          
-        </Layout>
-      </div>
-    );
   }
+  return (
+    <div>
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>
+          {routes}
+        </Suspense>
+      </Layout>
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
@@ -78,4 +68,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(app));
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import {connect} from 'react-redux';
 
 import Aux from '../Auxiliary/Auxiliary';
@@ -6,34 +6,30 @@ import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
-class Layout extends Component {
-    state = {
-        showSideDrawer: false
-    }
-    sideDrawerClosedHandler = () => {
+const layout = props => {
+    const [sideDrawerIsVisible, setSideDrawerIsVisible] = useState(false);
+   
+    const sideDrawerClosedHandler = () => {
       this.setState({showSideDrawer: false});
     }
-    sideDrawerToggleHandler = () => {
-        this.setState((prevState) => {
-            return {showSideDrawer: !prevState.showSideDrawer};
-        });
+    const sideDrawerToggleHandler = () => {
+        setSideDrawerIsVisible(!sideDrawerIsVisible);
     }
-    render () {
         return (
             <Aux>
                 <Toolbar
-                    isAuth={this.props.isAuthenticated}
-                    drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                    isAuth={props.isAuthenticated}
+                    drawerToggleClicked={sideDrawerToggleHandler}/>
                 <SideDrawer
-                  isAuth={this.props.isAuthenticated} 
-                  open={this.state.showSideDrawer}
-                  closed={this.sideDrawerClosedHandler}/>
+                  isAuth={props.isAuthenticated} 
+                  open={sideDrawerIsVisible}
+                  closed={sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
-                    {this.props.children}
+                    {props.children}
                 </main>
             </Aux>
         );
-    }
+
 }
 
 const mapStateToProps = state => {
@@ -41,4 +37,4 @@ const mapStateToProps = state => {
         isAuthenticated: state.auth.token !== null
     }
 }
-export default connect(mapStateToProps)(Layout);
+export default connect(mapStateToProps)(layout);
